778bea92106018c8f269d7348d9af34162de9fec
Olivier Laviale <olivier.laviale@gmail.com>
2023-03-06 23:56:08 +0100

    Make sure the query returns objects

--- lib/ActiveRecord/HasManyRelation.php
+++ lib/ActiveRecord/HasManyRelation.php
@@ -13,6 +13,8 @@ namespace ICanBoogie\ActiveRecord;
 
 use ICanBoogie\ActiveRecord;
 
+use PDO;
+
 use function ICanBoogie\pluralize;
 
 /**
@@ -74,6 +76,9 @@ class HasManyRelation extends Relation
         $r2_model = $this->ensure_model($r2->related);
 
         $q = $related->select("`{alias}`.*");
+        // Because of the select, we need to set the mode otherwise an array would be
+        // fetched instead of an object.
+        $q->mode(PDO::FETCH_CLASS, $related->activerecord_class);
         $q->join("INNER JOIN `{$through->name}` ON `{$through->name}`.{$r2->local_key} = `{$r2_model->alias}`.{$related->primary}");
         $q->join("INNER JOIN `{$owner->name}` `{$owner->alias}` ON `{$through->name}`.{$r1->local_key} = `{$owner->alias}`.{$owner->primary}");
         $q->where("`{$owner->alias}`.{$owner->primary} = ?", $record->{$this->local_key});
