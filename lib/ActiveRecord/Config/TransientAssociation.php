9fd2be9b3e07599ac7261f7f0ede5e3d03be236e
Olivier Laviale <olivier.laviale@gmail.com>
2023-09-13 22:44:18 +0200

    Remove AssociationBuilder::belongs_to
    The belongs_to relationship is defined by the Schema

--- lib/ActiveRecord/Config/TransientAssociation.php
+++ lib/ActiveRecord/Config/TransientAssociation.php
@@ -11,11 +11,9 @@ final class TransientAssociation
 {
     /**
      * @param array<TransientHasManyAssociation> $has_many
-     * @param array<TransientBelongsToAssociation> $belongs_to
      */
     public function __construct(
         public readonly array $has_many,
-        public readonly array $belongs_to,
     ) {
     }
 }
