f9f44a4f2c36a1c0d99a1af9618788913a1af30f
Olivier Laviale <olivier.laviale@gmail.com>
2023-09-30 12:49:00 +0200

    Tidy

--- lib/ActiveRecord/ModelIterator.php
+++ lib/ActiveRecord/ModelIterator.php
@@ -2,15 +2,17 @@
 
 namespace ICanBoogie\ActiveRecord;
 
+use ICanBoogie\ActiveRecord;
+
 /**
  * An interface to provide a model iterator.
  */
 interface ModelIterator
 {
     /**
-     * @template T of Model
+     * @template T of ActiveRecord
      *
-     * @return iterable<class-string<T>, (callable(): T)>
+     * @return iterable<class-string<T>, (callable(): Model<scalar|scalar[],T>)>
      */
     public function model_iterator(): iterable;
 }
